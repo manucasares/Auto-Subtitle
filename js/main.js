75e5e9f802a2cd29b0eeae2445ae29261440ea63
Manuel Casares <manucasaresrojas@gmail.com>
2021-08-23 18:24:02 -0300

    añadido '¿' dentro de RegExp de capitalizeFirstSenteceWord

--- js/main.js
+++ js/main.js
@@ -1,110 +1,82 @@
 import { capitalized } from './data/capitalized.js';
 
-const number = document.querySelector( '#number' );
-const input = document.querySelector( '#input' );
-const output = document.querySelector( '#output' );
+const number = document.querySelector('#number');
+const input = document.querySelector('#input');
+const output = document.querySelector('#output');
 
-let limitChar = Number( number.value );
+let limitChar = Number(number.value);
 
-number.addEventListener( 'input', ( e ) => {
-    limitChar = Number( e.target.value );
-    console.log(limitChar);
-} )
+number.addEventListener('input', (e) => {
+	limitChar = Number(e.target.value);
+	console.log(limitChar);
+});
 
-input.addEventListener( 'input', ( e ) => {
-    
-    // Agarra el value y  To lowercase
-    let inputValue = e.target.value.toLowerCase();
-    
-    // - Mayúsculas en nombres propios
-    inputValue = capitalizeNames( inputValue );
+input.addEventListener('input', (e) => {
+	// Agarra el value y  To lowercase
+	let inputValue = e.target.value.toLowerCase();
 
-    // - Mayúsculas despues de puntos, signos.
-    inputValue = capitalizeFirstSentenceWord( inputValue );
+	// - Mayúsculas en nombres propios
+	inputValue = capitalizeNames(inputValue);
 
-    // Separamos string en array de letras    
-    const splitted = inputValue.split('');
-    
-    createBreakLines( splitted );
+	// - Mayúsculas despues de puntos, signos.
+	inputValue = capitalizeFirstSentenceWord(inputValue);
 
-    output.value = splitted.join( '' );
-} )
+	// Separamos string en array de letras
+	const splitted = inputValue.split('');
 
-function capitalizeNames( value ) {
+	createBreakLines(splitted);
 
-    const capitalizedObject = capitalized.reduce( ( acc, cv ) => ({
-        ...acc,
-        [ cv.toLowerCase() ]: cv
-    }), {} );
+	output.value = splitted.join('');
+});
 
-    capitalized.forEach( element => {
-        element = element.toLowerCase();
+function capitalizeNames(value) {
+	const capitalizedObject = capitalized.reduce(
+		(acc, cv) => ({
+			...acc,
+			[cv.toLowerCase()]: cv,
+		}),
+		{}
+	);
 
-        const rgx = new RegExp( element, 'g' );
+	capitalized.forEach((element) => {
+		element = element.toLowerCase();
 
-        value = value.replace( rgx, capitalizedObject[ element ] );
-    });
+		const rgx = new RegExp(element, 'g');
 
-    return value;
+		value = value.replace(rgx, capitalizedObject[element]);
+	});
+
+	return value;
 }
 
-function capitalizeFirstSentenceWord( value ) {
-    const rgx = new RegExp( /(?<=[\.\?\!]\s*)[a-z]/, 'g' );
-    value = value.replace( rgx, (v) => v.toUpperCase());
-    return value;
+function capitalizeFirstSentenceWord(value) {
+	const rgx = new RegExp(/(?<=[\.\¿\?\!]\s*)[a-z]/, 'g');
+	value = value.replace(rgx, (v) => v.toUpperCase());
+	return value;
 }
 
-function createBreakLines ( splitted ) {
+function createBreakLines(splitted) {
+	let letterCounter = 0;
+	let breakCounter = 0;
 
-    let letterCounter = 0;
-    let breakCounter = 0;
+	const afterBreakLine = ['.', ',', '?', '!'];
 
-    const afterBreakLine = [ '.', ',', '?', '!' ];
+	splitted.forEach((letter, index) => {
+		// // - Saltos de linea por coma, punto, signos de pregunta, etc...
+		if (afterBreakLine.includes(letter) && letterCounter > limitChar / 2.5) {
+			letter = splitted[index + 1];
+			index++;
+			letterCounter = limitChar;
+		}
 
-    splitted.forEach( ( letter, index ) => {
-        
-        // // - Saltos de linea por coma, punto, signos de pregunta, etc...
-        if ( afterBreakLine.includes( letter ) && ( letterCounter > limitChar / 2.5 ) ) {
-            letter = splitted[ index + 1 ];
-            index++;
-            letterCounter = limitChar;
-        }
+		if (letterCounter >= limitChar && letter === ' ') {
+			splitted[index] = breakCounter % 2 ? '\n\n' : '\n';
 
-        if ( letterCounter >= limitChar && letter === ' ' ) {
+			breakCounter++;
+			// -1 para que despues con el ++ vuelva a 0
+			letterCounter = -1;
+		}
 
-            splitted[ index ] = ( breakCounter % 2 ) ? '\n\n' : '\n';
-
-            breakCounter++;
-            // -1 para que despues con el ++ vuelva a 0
-            letterCounter = -1;
-        }
-        
-        letterCounter++;
-    } )
+		letterCounter++;
+	});
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
